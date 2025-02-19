ce2c6c69eff4574d5301c8e30bfc2f60d906a1db
Subarno Neel <144299709+subarnoneel@users.noreply.github.com>
2025-02-19 10:12:12 +0600

    Add files via upload

---
+++ 206 lab 11/206 task 1.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Time is a great teacher, but unfortunately "
+                 "it kills all its pupils. Berlioz";
+    ofstream outfile("TEST.TXT");
+    for(int j=0; j<str.size(); j++)
+        outfile.put( str[j] );
+    cout << "File written\n";
+    return 0;
+}
